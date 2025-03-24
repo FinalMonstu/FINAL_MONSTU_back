262ee2ef2884232446ab5ed46f24bee5cc915de1
hosik99 <wnddl777@gmail.com>
2025-03-24 15:52:43 +0900

    Create kimice branch and commit changes

---
+++ src/main/java/com/icetea/MonStu/MonStuApplication.java
@@ -0,0 +1,13 @@
+package com.icetea.MonStu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonStuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonStuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icetea/MonStu/MonStuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icetea.MonStu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonStuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
