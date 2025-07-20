018f4bc248b80ed9be2c2a50acd6784c06da9ab9
hosik99 <wnddl777@gmail.com>
2025-07-20 15:39:16 +0900

    refactor(api): change PresetController endpoints, baseURL
    -PresetController baseURL 변경 /api/preset -> /api/v2/presets
    -PresetController 엔드포인트 변경
    	v1                  v2
    	GET  /lang       -> GET  /languages
    	GET  /coun       -> GET  /countries
    	GET  /mem/status -> GET  /member-statuses
    	GET  /mem/role   -> GET  /member-roles
    
    BREAKING CHANGE: PresetController 엔드포인트와 baseURL 변경

--- src/main/java/com/icetea/MonStu/api/v2/controller/PresetController.java
+++ src/main/java/com/icetea/MonStu/api/v2/controller/PresetController.java
@@ -20,8 +20,8 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v2/preset")
-@Tag(name = "Presetting API", description = "고정 요소 리스트 반환 API ex) 국가코드, 국가언어,언어코드")
+@RequestMapping("/api/v2/presets")
+@Tag(name = "Lookup API", description = "고정(프리셋) 값 조회용 API")
 public class PresetController {
 
     private final PresetService presetSvc;
@@ -32,7 +32,7 @@ public class PresetController {
             @ApiResponse(responseCode = "200", description = "반환 성공"),
             @ApiResponse(responseCode = "500", description = "서버 내부 오류")
     })
-    @GetMapping("/lang")
+    @GetMapping("/languages")
     public ResponseEntity<List<String>> getLanguageList(){
         List<String> result = presetSvc.getLanguageList();
         return result.isEmpty()
@@ -46,7 +46,7 @@ public class PresetController {
             @ApiResponse(responseCode = "200", description = "반환 성공"),
             @ApiResponse(responseCode = "500", description = "서버 내부 오류")
     })
-    @GetMapping("/coun")
+    @GetMapping("/countries")
     public ResponseEntity<List<String>> getCountryList() {
         List<String> result = presetSvc.getCountryList();
         return result.isEmpty()
@@ -60,7 +60,7 @@ public class PresetController {
             @ApiResponse(responseCode = "200", description = "반환 성공"),
             @ApiResponse(responseCode = "500", description = "서버 내부 오류")
     })
-    @GetMapping("/mem/status")
+    @GetMapping("/member-statuses")
     @PreAuthorize("hasRole(T(com.icetea.MonStu.enums.MemberRole).ADMIN.name())")
     public ResponseEntity<List<MemberStatus>> getMemberStatus() {
         List<MemberStatus> result = presetSvc.getMemberStatus();
@@ -75,7 +75,7 @@ public class PresetController {
             @ApiResponse(responseCode = "200", description = "반환 성공"),
             @ApiResponse(responseCode = "500", description = "서버 내부 오류")
     })
-    @GetMapping("/mem/role")
+    @GetMapping("/member-roles")
     @PreAuthorize("hasRole(T(com.icetea.MonStu.enums.MemberRole).ADMIN.name())")
     public ResponseEntity<List<MemberRole>> getMemberRole() {
         List<MemberRole> result = presetSvc.getMemberRole();
